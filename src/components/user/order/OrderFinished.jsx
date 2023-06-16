ae95658193415fa012bea579cbbd98463a541e48
luca-lagos <lagos.herrera.luca@gmail.com>
2023-06-16 13:48:28 -0300

    EntregaFinalLagos

--- src/components/user/order/OrderFinished.jsx
+++ src/components/user/order/OrderFinished.jsx
@@ -1,8 +1,38 @@
 import { useCart } from "../../../hooks/customHooks";
+import { useState, useEffect } from "react";
+import { Box, CircularProgress } from "@mui/material";
 
 const OrderFinished = () => {
+  const [loading, setLoading] = useState(false);
   const { orderId } = useCart();
-  return <div style={{marginTop: '300px'}}>THE ORDER {orderId} WAS FINISHED</div>;
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Box
+          sx={{
+            width: "100%",
+            height: "400px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress size={60} color="success" />
+        </Box>
+      ) : (
+        <div style={{ marginTop: "300px" }}>
+          THE ORDER {orderId} WAS FINISHED
+        </div>
+      )}
+    </>
+  );
 };
 
 export default OrderFinished;
